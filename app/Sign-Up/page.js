4b1c2b4ac7982b79d4ca468d0dbd9db6e727f63b
shaluka <shalukamahagamage@gmail.com>
2024-01-14 02:03:06 +0530

    new

---
+++ app/Sign-Up/page.js
@@ -0,0 +1,25 @@
+'use client'
+import "/styles/Signup.css";
+import Link from "next/link";
+
+
+function page() {
+    return (
+        <div className="container">
+            <h2>Create An Account</h2>
+            <input className="input" type="text" placeholder="First Name" />
+            <input className="input" type="text" placeholder="Last Name" />
+            <input className="input" type="text" placeholder="Registration No" />
+            <input className="input" type="email" placeholder="Email" />
+            <input className="input" type="text" placeholder="Phone No" />
+            <input className="input" type="text" placeholder="Username" />
+            <input className="input" type="password" placeholder="Password" />
+            <input className="input" type="password" placeholder="Re-type Password" />
+            <button className="btn">Sign Up</button>
+            <Link href='/' className="link2">Already have an account? Signin</Link>
+
+        </div>
+    )
+}
+
+export default page
--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+
 import { Inter } from 'next/font/google'
 import "/styles/global.css";
 
